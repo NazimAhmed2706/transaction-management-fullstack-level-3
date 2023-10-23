b2efd09431b4ebc6d400163c33392fe699cee4de
NazimAhmed2706 <a.nazimahmed@gmail.com>
2023-10-23 02:34:40 +0200

    Commit All

--- app-react/src/components/TransactionHistory.jsx
+++ app-react/src/components/TransactionHistory.jsx
@@ -15,10 +15,10 @@ function TransactionHistory() {
 
         axios
             .get('http://localhost:5000/transactions')
-            .then((res) => {
+            .then(async function (response) {
             // Response is the array of transactions
-            console.log("TransactionHistory.jsx= => TransactionHistory => GET http://localhost:5000/transactions : ", res.data);
-            const arrayOfTransactions = JSON.parse(res.data);
+            console.log("TransactionHistory.jsx= => TransactionHistory => GET http://localhost:5000/transactions : ", response.data);
+            const arrayOfTransactions = JSON.parse(response.data);
             console.log("TransactionHistory.jsx= => TransactionHistory => Array of Transactions Count : ", arrayOfTransactions.length);
 
             // Loop thru the array of transactions to make array of summary
@@ -32,23 +32,23 @@ function TransactionHistory() {
                     txnAmt = txnAmt * -1;
                 }
                 
-                var indexingTransactionSummary = "Transferred " + txnAmt + "$ " + txnDir + " account " + transaction.account_id + ".";
+                let indexingTransactionSummary = "Transferred " + txnAmt + "$ " + txnDir + " account " + transaction.account_id + ".";
 
                 // Add Balance Detail for latest transaction
                 
                 if(index === arrayOfTransactions.length - 1){
                     let accBalance = 0;
-                    axios
+                    await axios
                         .get('http://localhost:5000/accounts/'+transaction.account_id)
-                        .then((res) => {
+                        .then(function (response) {
                             // Response is the account details
-                            console.log("TransactionHistory.jsx= => TransactionHistory => GET http://localhost:5000/accounts/" + transaction.account_id + " : ", res.data);
-                            const accountDetails = JSON.parse(res.data);
+                            console.log("TransactionHistory.jsx= => TransactionHistory => GET http://localhost:5000/accounts/" + transaction.account_id + " : ", response.data);
+                            const accountDetails = JSON.parse(response.data);
                             accBalance = +accountDetails.balance;
                             console.log("TransactionHistory.jsx= => TransactionHistory => Account Balance " + accBalance + "$");
+                            indexingTransactionSummary = indexingTransactionSummary + "\nThe current account balance is " + accBalance + "$."
                         });
-                    console.log("TransactionHistory.jsx= => TransactionHistory => Account Balance " + accBalance + "$");
-                    indexingTransactionSummary = indexingTransactionSummary + "\nThe current account balance is " + accBalance + "$."
+                    
                 }
                 tempTransactionSummary.push(indexingTransactionSummary)
                 console.log("TransactionHistory.jsx= => TransactionHistory => ", indexingTransactionSummary);
