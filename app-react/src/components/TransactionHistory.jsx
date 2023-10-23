6b8c437cc4c58f2d6f158bd289dda0e24257567e
NazimAhmed2706 <a.nazimahmed@gmail.com>
2023-10-23 02:17:58 +0200

    Commit All

--- app-react/src/components/TransactionHistory.jsx
+++ app-react/src/components/TransactionHistory.jsx
@@ -1,27 +1,72 @@
 // Import NPM Modules
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import Stack from 'react-bootstrap/Stack';
+import axios from "axios";
 
 console.log("TransactionHistory.jsx= => NPM modules imported");
 
+
 function TransactionHistory() {
+    const [transactionSummary , setTransactionSummary] = useState([]);
+    
     useEffect(() => {
         console.log("TransactionHistory.jsx= => TransactionHistory - function call");
-    }, []);
+        const tempTransactionSummary = [];
 
-    const getTransactions = (event) => {
-        /*axios.get('http://localhost:5000/transactions')
-      .then((res) => {
-        console.log(res)
-      })
-      .catch(err => console.error(err));*/
-    };
+        axios
+            .get('http://localhost:5000/transactions')
+            .then((res) => {
+            // Response is the array of transactions
+            console.log("TransactionHistory.jsx= => TransactionHistory => GET http://localhost:5000/transactions : ", res.data);
+            const arrayOfTransactions = JSON.parse(res.data);
+            console.log("TransactionHistory.jsx= => TransactionHistory => Array of Transactions Count : ", arrayOfTransactions.length);
+
+            // Loop thru the array of transactions to make array of summary
+            for (let index = 0; index < arrayOfTransactions.length; index++) {
+                const transaction = arrayOfTransactions[index];
+                console.log("TransactionHistory.jsx= => TransactionHistory => Transactions : ", index, transaction);
+                var txnDir = "to";
+                var txnAmt = + transaction.amount; 
+                if ((transaction.amount) < 0) {
+                    txnDir = "from";
+                    txnAmt = txnAmt * -1;
+                }
+                
+                var indexingTransactionSummary = "Transferred " + txnAmt + "$ " + txnDir + " account " + transaction.account_id + ".";
+
+                // Add Balance Detail for latest transaction
+                
+                if(index === arrayOfTransactions.length - 1){
+                    let accBalance = 0;
+                    axios
+                        .get('http://localhost:5000/accounts/'+transaction.account_id)
+                        .then((res) => {
+                            // Response is the account details
+                            console.log("TransactionHistory.jsx= => TransactionHistory => GET http://localhost:5000/accounts/" + transaction.account_id + " : ", res.data);
+                            const accountDetails = JSON.parse(res.data);
+                            accBalance = +accountDetails.balance;
+                            console.log("TransactionHistory.jsx= => TransactionHistory => Account Balance " + accBalance + "$");
+                        });
+                    console.log("TransactionHistory.jsx= => TransactionHistory => Account Balance " + accBalance + "$");
+                    indexingTransactionSummary = indexingTransactionSummary + "\nThe current account balance is " + accBalance + "$."
+                }
+                tempTransactionSummary.push(indexingTransactionSummary)
+                console.log("TransactionHistory.jsx= => TransactionHistory => ", indexingTransactionSummary);
+            }
+            tempTransactionSummary.reverse();
+            setTransactionSummary(tempTransactionSummary);
+            })
+            .catch(err => console.error(err));
+    },[]);
 
     return (
         <Stack direction="vertical" className='mt-3' gap={3}>
-            <div className="border rounded p-3">Transaction 1</div>
-            <div className="border rounded p-3">Transaction 2</div>
-            <div className="border rounded p-3">Transaction 3</div>
+            {
+                transactionSummary.map((transaction)=>{
+                    return <div className="border rounded p-3" key={transaction} style={{whiteSpace: 'pre-line'}}>{transaction}</div>
+                })
+            }
+
         </Stack>
     );
 }
