d8dfc4714f74f3c5b6ad7b8d0e49a4b70fa1925e
NazimAhmed2706 <a.nazimahmed@gmail.com>
2023-10-24 20:02:53 +0200

    Commit All

--- app-react/src/components/NewTransaction.jsx
+++ app-react/src/components/NewTransaction.jsx
@@ -4,33 +4,38 @@ import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Form from 'react-bootstrap/Form';
-
-
-
 console.log("NewTransaction.jsx= => NPM modules imported");
 
 function NewTransaction() {
   console.log("NewTransaction.jsx= => NewTransaction - function call");
 
-  const[accId, setAccId] = useState("");
-  const[amt, setAmt] = useState("");
-  const[validated, setValidation] = useState(false);
+  const initialValues = {account_id: "", amount: ""};
+  const [formValues, setFormValues] = useState(initialValues);
+  const [validated, setValidation] = useState(false);
   console.log("NewTransaction.jsx= => NewTransaction => Default state assigned");
 
-  const handleSubmit = (event) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name] : value });
+    console.log(formValues);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
     // Perform action on button click
     console.log("NewTransaction.jsx= => NewTransaction => handleSubmit - function call");
 
-    console.log("NewTransaction.jsx= => NewTransaction => handleSubmit => Account Id is ", accId);
-    console.log("NewTransaction.jsx= => NewTransaction => handleSubmit => Amount is ", amt);
+    console.log("NewTransaction.jsx= => NewTransaction => handleSubmit => Account Id is ", initialValues.accountId);
+    console.log("NewTransaction.jsx= => NewTransaction => handleSubmit => Amount is ", initialValues.amount);
 
     const accountIDRegExp = /^[a-f0-9]{8}\-[a-f0-9]{4}\-4[a-f0-9]{3}\-[a-f0-9]{4}\-[a-f0-9]{12}/;
-    if(! accountIDRegExp.test(accId)){
-      console.log("NewTransaction.jsx= => NewTransaction => Invalid Account Id: ", accId);
+    if(! accountIDRegExp.test(initialValues.accountId)){
+      console.log("NewTransaction.jsx= => NewTransaction => Invalid Account Id: ", initialValues.accountId);
       return;
     }
 
-    const form = event.currentTarget;
+    const form = e.currentTarget;
     if (form.checkValidity() === false){
       console.log("NewTransaction.jsx= => NewTransaction => handleSubmit => Error: Validation Failed!!");
       return;
@@ -41,8 +46,8 @@ function NewTransaction() {
 
     // Post the transaction request
     axios.post('http://localhost:5000/transactions', {
-      "account_id" : accId,
-      "amount": amt,
+      "account_id" : initialValues.accountId,
+      "amount": initialValues.amount,
     })
     .then((res) => {
       // Response is the transaction
@@ -52,16 +57,17 @@ function NewTransaction() {
   };
 
   return (
-    <Form className="border rounded mt-3 p-1" noValidate validated={validated} onSubmit={handleSubmit}>
+    // noValidate validated={validated} 
+    <Form className="border rounded mt-3 p-1" onSubmit={handleSubmit}> 
       <Form.Group as={Row} className="m-1">
         <Form.Label>Account ID:</Form.Label>
-        <Form.Control required type="string" data-type="account-id" placeholder="Enter Account ID" value={accId} onChange={(e)=> setAccId(e.target.value)} />
-        <Form.Control.Feedback type="invalid">Please enter a valid Account ID</Form.Control.Feedback>
+        <Form.Control type="text" data-type="account-id" placeholder="Enter Account ID" name="account_id" value={formValues.account_id} onChange={handleChange} />
+        {/* <Form.Control.Feedback type="invalid">Please enter a valid Account ID</Form.Control.Feedback> */}
       </Form.Group>
       <Form.Group as={Row} className="m-1">
         <Form.Label>Amount:</Form.Label>
-        <Form.Control required type="number" data-type="amount" placeholder="Enter Amount" value={amt} onChange={(e)=> setAmt(e.target.value)} />
-        <Form.Control.Feedback type="invalid">Please enter a valid amount</Form.Control.Feedback>
+        <Form.Control type="text" data-type="amount" placeholder="Enter Amount" name="amount" value={formValues.amount} onChange={handleChange} />
+        {/* <Form.Control.Feedback type="invalid">Please enter a valid amount</Form.Control.Feedback> */}
       </Form.Group>
       <div className='d-flex justify-content-center mt-3'>
         <Button type="submit" data-type="transaction-submit" variant="primary">Submit</Button>
